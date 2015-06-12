c4cb457d867ea6bd533a1a10ab151de67b985573
jack7671541 <jack7671541@gmail.com>
2015-06-12 23:45:46 +0800

    Add

--- Calculator/src/tw/edu/npu/mis/Calculator.java
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -50,11 +50,13 @@ public class Calculator {
         return null;
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
+ 
 }
+
+        
+         
+            
+    
+   
+
+
