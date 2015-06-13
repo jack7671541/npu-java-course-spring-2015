765098c9759a8e19435cb456470b1e8be06a569f
jack7671541 <jack7671541@gmail.com>
2015-06-13 17:12:19 +0800

    new

--- Calculator/src/tw/edu/npu/mis/Calculator.java
+++ Calculator/src/tw/edu/npu/mis/Calculator.java
@@ -8,7 +8,9 @@ package tw.edu.npu.mis;
 /**
  * The model class of the calculator application.
  */
-public class Calculator {
+public class Calculator  extends java.util.Observable{
+    String Mata = "",j;
+    int c ,d;
     
     /**
      * The available operators of the calculator.
@@ -35,22 +37,145 @@ public class Calculator {
     
     public void appendDigit(int digit) {
         // TODO code application logic here
+        Mata += String.valueOf(digit);
+        getDisplay();
     }
     
     public void appendDot() {
         // TODO code application logic here
+        
     }
     
     public void performOperation(Operator operator) {
         // TODO code application logic here
+          switch (operator) {
+            case PLUS:
+               c = Integer.parseInt(Mata);
+               Mata = "";
+               getDisplay();
+               j = "+";
+                break;
+            case MINUS:
+               c = Integer.parseInt(Mata);
+               Mata = "";
+               getDisplay();
+               j = "-";
+                break;
+            case TIMES:
+               c = Integer.parseInt(Mata);
+               Mata = "";
+               getDisplay();
+              j = "*";
+                break;
+            case OVER:
+                c = Integer.parseInt(Mata);
+               Mata = "";
+               getDisplay();
+               j = "/";
+             case CLEAR:
+               Mata = "";
+               getDisplay();
+                 break;
+              case  PLUS_MINUS:
+                c = Integer.parseInt(Mata);
+               Mata = "";
+               getDisplay();
+               j = "+/-";
+            case EQUAL:
+           
+                if(j == "+")
+                {
+                    d= Integer.parseInt(Mata);
+                    Mata = String.valueOf(c+d);
+                    System.out.print(Mata);
+                    getDisplay();
+                    Mata="";
+                }
+                 if(j == "-")
+                {
+                    d= Integer.parseInt(Mata);
+                    Mata = String.valueOf(c-d);
+                    System.out.print(Mata);
+                    getDisplay();
+                    Mata="";
+                }
+                  if(j == "*")
+                {
+                    d= Integer.parseInt(Mata);
+                    Mata = String.valueOf(c*d);
+                    System.out.print(Mata);
+                    getDisplay();
+                    Mata="";
+                }
+                   if(j == "/")
+                {
+                    d= Integer.parseInt(Mata);
+                    Mata = String.valueOf(c/d);
+                    System.out.print(Mata);
+                    getDisplay();
+                    Mata="";
+                }
+                   
+                break;
+        }
     }
     
     public String getDisplay() {
         // TODO code application logic here
+        setChanged();
+       notifyObservers(Mata);
         return null;
+        
     }
+      public void  aa (String AAAA)
+    {
+        if(AAAA =="+") performOperation(Operator.PLUS);
+        if(AAAA =="-") performOperation(Operator.MINUS);
+        if(AAAA =="*") performOperation(Operator.TIMES);
+        if(AAAA =="/") performOperation(Operator.OVER);
+        if(AAAA =="c") performOperation(Operator.CLEAR);  
+        if(AAAA =="=") performOperation(Operator.EQUAL);
+         if(AAAA =="+/-") performOperation(Operator. PLUS_MINUS);
+    }
+         /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                 Calculator model = new Calculator();
+              NewJFrame view = new NewJFrame(model);
+              view.setVisible(true);
+              model.addObserver(view);
+            }
+        });
 
  
+    }
 }
 
         
--- Calculator/src/tw/edu/npu/mis/NewJFrame.java
+++ Calculator/src/tw/edu/npu/mis/NewJFrame.java
@@ -6,13 +6,15 @@
 package tw.edu.npu.mis;
 
 import java.awt.Font;
+import java.util.Observable;
 
 /**
  *
  * @author STP
  */
-public class NewJFrame extends javax.swing.JFrame  {
-    
+public class NewJFrame extends javax.swing.JFrame implements java.util.Observer {
+        Calculator OB;
+        
         double firstnum;
         double secondnum;
         double third;
@@ -22,7 +24,8 @@ public class NewJFrame extends javax.swing.JFrame  {
     /**
      * Creates new form NewJFrame
      */
-    public NewJFrame() {
+    public NewJFrame( Calculator aa) {
+         OB = aa;
         initComponents();
     }
 
@@ -53,6 +56,7 @@ public class NewJFrame extends javax.swing.JFrame  {
         jButton15 = new javax.swing.JButton();
         jButton16 = new javax.swing.JButton();
         jButton17 = new javax.swing.JButton();
+        jButton18 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
@@ -134,6 +138,11 @@ public class NewJFrame extends javax.swing.JFrame  {
         });
 
         jButton11.setText(".");
+        jButton11.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton11ActionPerformed(evt);
+            }
+        });
 
         jButton12.setText("=");
         jButton12.addActionListener(new java.awt.event.ActionListener() {
@@ -174,6 +183,13 @@ public class NewJFrame extends javax.swing.JFrame  {
         jButton17.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton17ActionPerformed(evt);
+            }
+        });
+
+        jButton18.setText("+/-");
+        jButton18.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton18ActionPerformed(evt);
             }
         });
 
@@ -218,14 +234,16 @@ public class NewJFrame extends javax.swing.JFrame  {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButton9)
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton17))
+                                .addComponent(jButton17)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton18))
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jButton10)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jButton11)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jButton12)))))
-                .addContainerGap(185, Short.MAX_VALUE))
+                .addContainerGap(179, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -251,7 +269,8 @@ public class NewJFrame extends javax.swing.JFrame  {
                     .addComponent(jButton7)
                     .addComponent(jButton8)
                     .addComponent(jButton9)
-                    .addComponent(jButton17))
+                    .addComponent(jButton17)
+                    .addComponent(jButton18))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton10)
@@ -264,82 +283,72 @@ public class NewJFrame extends javax.swing.JFrame  {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-          String takein; 
-        takein = jTextField1.getText() + jButton1.getText();
+         OB.appendDigit(Integer.parseInt(jButton1.getText()));
         jButton1.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton2.getText();
+        OB.appendDigit(Integer.parseInt(jButton2.getText()));
         jButton2.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+         
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton3.getText();
+        OB.appendDigit(Integer.parseInt(jButton3.getText()));
         jButton3.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+         
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton4.getText();
+        
         jButton4.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+        OB.appendDigit(Integer.parseInt(jButton4.getText()));
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton5.getText();
+       
         jButton5.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+        OB.appendDigit(Integer.parseInt(jButton5.getText()));
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton6.getText();
+       
         jButton6.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+         OB.appendDigit(Integer.parseInt(jButton6.getText()));
     }//GEN-LAST:event_jButton6ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton7.getText();
+        
         jButton7.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+        OB.appendDigit(Integer.parseInt(jButton7.getText()));
     }//GEN-LAST:event_jButton7ActionPerformed
 
     private void jButton8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton8ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton8.getText();
+       
         jButton8.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+       OB.appendDigit(Integer.parseInt(jButton8.getText()));
     }//GEN-LAST:event_jButton8ActionPerformed
 
     private void jButton9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton9ActionPerformed
         // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton9.getText();
+       
         jButton9.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+        OB.appendDigit(Integer.parseInt(jButton9.getText()));
     }//GEN-LAST:event_jButton9ActionPerformed
 
     private void jButton10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton10ActionPerformed
             // TODO add your handling code here:
-        String takein; 
-        takein = jTextField1.getText() + jButton10.getText();
+        
         jButton10.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText(takein);
+        OB.appendDigit(Integer.parseInt(jButton10.getText()));
     }//GEN-LAST:event_jButton10ActionPerformed
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
@@ -348,94 +357,53 @@ public class NewJFrame extends javax.swing.JFrame  {
 
     private void jButton14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton14ActionPerformed
         // TODO add your handling code here:
-        firstnum = Double.parseDouble( jTextField1.getText());
-        jButton14.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText("");
-         open = "-";
+        OB.aa(jButton14.getText());
     }//GEN-LAST:event_jButton14ActionPerformed
 
     private void jButton13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton13ActionPerformed
         // TODO add your handling code here:
-         firstnum = Double.parseDouble( jTextField1.getText());
-        jButton13.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText("");
-         open = "+";
+         OB.aa(jButton13.getText());
     }//GEN-LAST:event_jButton13ActionPerformed
 
     private void jButton15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton15ActionPerformed
         // TODO add your handling code here:
-         firstnum = Double.parseDouble( jTextField1.getText());
-        jButton15.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText("");
-         open = "*";
+        OB.aa(jButton15.getText());
     }//GEN-LAST:event_jButton15ActionPerformed
 
     private void jButton16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton16ActionPerformed
         // TODO add your handling code here:
-         firstnum = Double.parseDouble( jTextField1.getText());
-        jButton16.setFont(new Font("Consolas", Font.BOLD, 32));
-         jTextField1.setText("");
-         open = "/";
+         OB.aa(jButton16.getText());
     }//GEN-LAST:event_jButton16ActionPerformed
 
     private void jButton17ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton17ActionPerformed
         // TODO add your handling code here:
-        jTextField1.setText("");
-        jButton17.setFont(new Font("Consolas", Font.BOLD, 32));
+        OB.aa(jButton17.getText());
     }//GEN-LAST:event_jButton17ActionPerformed
 
     private void jButton12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton12ActionPerformed
         // TODO add your handling code here:
-        switch (open) {
-            case "+":
-                jTextField1.setText(String.valueOf(firstnum + Double.parseDouble(jTextField1.getText())));
-                break;
-            case "-":
-                jTextField1.setText(String.valueOf(firstnum - Double.parseDouble(jTextField1.getText())));
-                break;
-            case "*":
-                jTextField1.setText(String.valueOf(firstnum * Double.parseDouble(jTextField1.getText())));
-                break;
-            case "/":
-                jTextField1.setText(String.valueOf(firstnum / Double.parseDouble(jTextField1.getText())));
-                break;
-        }
+        OB.aa(jButton12.getText());
     }//GEN-LAST:event_jButton12ActionPerformed
+
+    private void jButton18ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton18ActionPerformed
+        // TODO add your handling code here:
+        OB.aa(jButton18.getText());
+    }//GEN-LAST:event_jButton18ActionPerformed
+
+    private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
+        // TODO add your handling code here:
+        jButton11.setFont(new Font("Consolas", Font.BOLD, 32));
+        OB.appendDigit(Integer.parseInt(jButton11.getText()));
+    }//GEN-LAST:event_jButton11ActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new NewJFrame().setVisible(true);
-            }
-        });
+        public void update(Observable o, Object arg) {
+        jTextField1.setText(arg.toString());
+    
     }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
@@ -447,6 +415,7 @@ public class NewJFrame extends javax.swing.JFrame  {
     private javax.swing.JButton jButton15;
     private javax.swing.JButton jButton16;
     private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton18;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
@@ -458,3 +427,4 @@ public class NewJFrame extends javax.swing.JFrame  {
     private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
+
